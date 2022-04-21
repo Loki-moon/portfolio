cb3af682a4adde7a6d3cd269cdf9c74208bbd8cd
Jun-Yong Moon <79559873+Loki-moon@users.noreply.github.com>
2022-04-21 15:55:28 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["프로젝트 매니저(PM)", "연구원(Researcher)", "개발자(Developer)", "프로덕트 오너(PO)"],
+        strings: ["프로덕트 매니저(PM)", "연구원(Researcher)", "UI 개발자(UI Developer)", "프로덕트 오너(PO)"],
         typeSpeed: 100,
         backSpeed: 80,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["프로젝트 매니저(PM)", "연구원(Researcher)", "개발자(Developer)", "프로덕트 오너(PO)"],
+        strings: ["프로덕트 매니저(PM)", "연구원(Researcher)", "UI 개발자(UI Developer)", "프로덕트 오너(PO)"],
         typeSpeed: 100,
         backSpeed: 80,
         loop: true
